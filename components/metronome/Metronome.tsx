d3f562f4c0297465884b53e9d6c846840b1f0ac0
flotischoti <hello@vegan.bingo>
2023-01-08 20:16:14 +0100

    Adding button controls, twMerge dependency

--- components/metronome/Metronome.tsx
+++ components/metronome/Metronome.tsx
@@ -2,25 +2,73 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faCheck } from '@fortawesome/free-solid-svg-icons'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, MouseEvent, useEffect, useState } from 'react'
 import styles from './metronome.module.scss'
+import MainButton from '../shared/Button'
 
 export interface Metronome {
   title: string
   bpm: number
+  isPlaying: boolean
+  beats: number
 }
 
+const m: Metronome = {
+  title: 'Goldfinger - Superman',
+  bpm: 110,
+  isPlaying: false,
+  beats: 4,
+}
+
+const ts: number[] = []
+const maxBpm = 300
+const minBpm = 20
+
 export default function Metronome() {
-  const m: Metronome = {
-    title: 'Goldfinger - Superman',
-    bpm: 110,
-  }
   const [isEditTitle, setEditTitle] = useState(false)
   const [metronome, setMetronome] = useState(m)
+  const [tapSequence, setTapSequence] = useState(ts)
+
+  useEffect(() => calculateBpmFromTaps(), [JSON.stringify(tapSequence)])
 
   const changeBpm = (e: ChangeEvent<HTMLInputElement>) => {
-    m.bpm = +e.target.value
-    setMetronome(m)
+    setMetronome({ ...metronome, bpm: +e.target.value })
+  }
+
+  const decrease = (e: MouseEvent<HTMLButtonElement>) => {
+    setMetronome({ ...metronome, bpm: metronome.bpm - 1 })
+  }
+
+  const increase = (e: MouseEvent<HTMLButtonElement>) => {
+    setMetronome({ ...metronome, bpm: metronome.bpm + 1 })
+  }
+
+  const playPause = (e: MouseEvent<HTMLButtonElement>) => {
+    setMetronome({ ...metronome, isPlaying: !metronome.isPlaying })
+  }
+
+  const handleTap = (e: MouseEvent<HTMLButtonElement>) => {
+    const currentClickTimeStamp = Date.now()
+    if (
+      tapSequence.length == 0 ||
+      (currentClickTimeStamp - tapSequence.at(-1)) / 1000 > 2
+    ) {
+      setTapSequence([currentClickTimeStamp])
+    } else {
+      setTapSequence([...tapSequence, currentClickTimeStamp])
+    }
+  }
+
+  const calculateBpmFromTaps = (): void => {
+    if (tapSequence.length > 1) {
+      const timeWindow =
+        tapSequence.at(-1) -
+        tapSequence.at(Math.min(tapSequence.length, metronome.beats) * -1)
+      const taps = Math.min(tapSequence.length, metronome.beats)
+      const newBpm = Math.floor((taps / timeWindow) * 60000)
+      const normalizedBpm = Math.min(Math.max(newBpm, minBpm), maxBpm)
+      setMetronome({ ...metronome, bpm: normalizedBpm })
+    }
   }
 
   return (
@@ -58,12 +106,39 @@ export default function Metronome() {
           <div id="metronomeBpmControls-1" className="w-full px-4">
             <input
               type="range"
-              min="1"
-              max="250"
+              min={minBpm}
+              max={maxBpm}
               value={metronome.bpm}
               onChange={changeBpm}
               className={`${styles.slider} appearance-none w-full h-0.5 mb-6 cursor-pointer bg-blue-200 rounded-lg`}
             />
+            <div className="flex justify-between">
+              <MainButton
+                onClick={decrease}
+                className="w-1/6 rounded-sm border-black-600"
+              >
+                -
+              </MainButton>
+              <MainButton
+                onClick={playPause}
+                className="w-2/6 rounded-sm border-black-600"
+              >
+                {metronome.isPlaying ? 'Pause' : 'Play'}
+              </MainButton>
+              <MainButton
+                onClick={increase}
+                className="w-1/6 rounded-sm border-black-600"
+              >
+                +
+              </MainButton>
+            </div>
+            <br />
+            <MainButton
+              onClick={handleTap}
+              className="w-full h-20 rounded-sm border-black-600"
+            >
+              Tap
+            </MainButton>
           </div>
         </div>
       </div>
--- components/shared/button.tsx
+++ components/shared/button.tsx
@@ -1,8 +1,21 @@
-export default function MainButton({ children, color }) {
+'use client'
+
+import { MouseEvent } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export default function MainButton({
+  children,
+  onClick = () => {},
+  className = '',
+}: {
+  children: React.ReactNode
+  onClick?: (e: MouseEvent<HTMLButtonElement>) => void
+  className?: string
+}) {
   const style = {
     mobile: ['px-4', 'text-blue-400', 'py-2'],
     sm: ['sm:px-6', 'text-blue-600'],
-    general: ['uppercase', 'border-2'],
+    font: ['uppercase', 'tracking-widest'],
     hover: [
       'hover:bg-black',
       'hover:bg-opacity-5',
@@ -12,11 +25,15 @@ export default function MainButton({ children, color }) {
       'duration-150',
       'ease-in-out',
     ],
-    shape: ['rounded-full'],
-    color: color ? [`border-${color}-600`] : [],
+    border: ['rounded-full', 'border-2', 'border-orange-200'],
+    shoadow: ['shadow-lg'],
   }
 
   const classes = Object.values(style).flat().join(' ')
 
-  return <button className={classes}>{children}</button>
+  return (
+    <button className={twMerge(classes, className)} onClick={onClick}>
+      {children}
+    </button>
+  )
 }
