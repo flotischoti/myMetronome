dbea88d5a0199f0d503d508ace84d9de18d3ec31
flotischoti <hello@vegan.bingo>
2023-11-04 17:58:22 +0100

    Recent use revalidate attempt

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,9 @@ import Loading from './loading'
 import { cookies } from 'next/headers'
 import Sidebar from '../components/sidebar/Sidebar'
 import { getUserAttrFromToken, verifyToken } from './api/util'
-import Modal from '../components/searchModal/modal'
-import { StoredMetronome } from '../components/metronome/Metronome'
-import * as metronomeDb from '../db/metronome'
+
 import { Inter, Pacifico } from 'next/font/google'
 
-export const revalidate = 0
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
@@ -30,18 +27,9 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  async function getRecent(
-    userId: number
-  ): Promise<[number, StoredMetronome[]]> {
-    return await metronomeDb.list(userId, 5, 0, 'lastOpened', 'desc', undefined)
-  }
-
   const cookieStore = cookies()
   const token = cookieStore.get('token')
   const userName = await getUserAttrFromToken<string>(token?.value, 'name')
-  const userId = await getUserAttrFromToken(token?.value)
-  const isLoggedIn = userName ? true : false
-  const [count, recentMetronomes] = userId ? await getRecent(userId) : [0, []]
 
   return (
     <html lang="en" className={`${pacifico.variable} ${inter.variable} `}>
@@ -50,14 +38,13 @@ export default async function RootLayout({
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col h-screen justify-between gap-0">
           <div className="h-full flex flex-col">
-            <Navbar isLoggedIn={isLoggedIn} />
+            <Navbar />
             <main className="h-full">
               <Suspense fallback={<Loading />}>{children}</Suspense>
             </main>
           </div>
         </div>
         <Sidebar userName={userName} />
-        <Modal recentCount={count} recentMetronomes={recentMetronomes} />
       </body>
     </html>
   )
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,57 +1,72 @@
-import {
-  IconCone2,
-  IconMenu2,
-  IconPlus,
-  IconUserCircle,
-} from '@tabler/icons-react'
+import { IconCone2, IconPlus, IconUserCircle } from '@tabler/icons-react'
 import Link from 'next/link'
 import NavbarAccountDropdown from './NavbarAccountDropdown'
 import NavbarLoggedIn from './NavbarLoggedIn'
+import Modal from '../searchModal/modal'
+import { cookies } from 'next/headers'
+import { getUserAttrFromToken } from '@/app/api/util'
+import { StoredMetronome } from '../metronome/Metronome'
+import * as metronomeDb from '../../db/metronome'
 
-export default function Navbar({ isLoggedIn }: { isLoggedIn: boolean }) {
+export const revalidate = 0
+
+async function getRecent(userId: number): Promise<[number, StoredMetronome[]]> {
+  return await metronomeDb.list(userId, 5, 0, 'lastOpened', 'desc', undefined)
+}
+
+export default async function Navbar() {
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')
+  const userId = await getUserAttrFromToken(token?.value)
+  const [count, recentMetronomes] = userId ? await getRecent(userId) : [0, []]
   return (
-    <div className="navbar bg-base-100 shadow sticky top-0 z-10">
-      <div className="flex-1 gap-2">
-        <Link
-          href="/"
-          prefetch={true}
-          className="normal-case text-lg sm:text-xl flex items-center gap-1"
-        >
-          <IconCone2 />
-          <h1 className="font-cursive">Metronomes</h1>
-        </Link>
-        <Link className="btn btn-accent mx-1" href="/metronome/new">
-          <IconPlus />
-          <span>New</span>
-        </Link>
+    <>
+      {userId && (
+        <Modal recentCount={count} recentMetronomes={recentMetronomes} />
+      )}
+      <div className="navbar bg-base-100 shadow sticky top-0 z-10">
+        <div className="flex-1 gap-2">
+          <Link
+            href="/"
+            prefetch={true}
+            className="normal-case text-lg sm:text-xl flex items-center gap-1"
+          >
+            <IconCone2 />
+            <h1 className="font-cursive">Metronomes</h1>
+          </Link>
+          <Link className="btn btn-accent mx-1" href="/metronome/new">
+            <IconPlus />
+            <span>New</span>
+          </Link>
+        </div>
+        <div className="flex-none">
+          {userId ? (
+            <NavbarLoggedIn />
+          ) : (
+            <>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="link sm:hidden">
+                  <IconUserCircle size="36" stroke="1" />
+                </label>
+                <NavbarAccountDropdown />
+              </div>
+              <div className="hidden sm:flex pr-2">
+                <Link prefetch={true} href="/login" className="link link-hover">
+                  Login
+                </Link>
+                <div className="divider divider-horizontal mx-1"></div>
+                <Link
+                  prefetch={true}
+                  href="/register"
+                  className="link link-hover"
+                >
+                  Sign up
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      <div className="flex-none">
-        {isLoggedIn ? (
-          <NavbarLoggedIn />
-        ) : (
-          <>
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="link sm:hidden">
-                <IconUserCircle size="36" stroke="1" />
-              </label>
-              <NavbarAccountDropdown />
-            </div>
-            <div className="hidden sm:flex pr-2">
-              <Link prefetch={true} href="/login" className="link link-hover">
-                Login
-              </Link>
-              <div className="divider divider-horizontal mx-1"></div>
-              <Link
-                prefetch={true}
-                href="/register"
-                className="link link-hover"
-              >
-                Sign up
-              </Link>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
+    </>
   )
 }
