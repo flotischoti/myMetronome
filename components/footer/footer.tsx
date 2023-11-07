755f05d88a4e711aa4b84f78789b1a5f4090c8e7
flotischoti <hello@vegan.bingo>
2023-11-07 19:14:58 +0100

    footer responsive sizes adaption

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -7,30 +7,32 @@ import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <footer className="footer z-100 items-center p-3 sm:p-4 text-xs sm:text-sm bg-neutral text-neutral-content">
+    <footer className="footer select-none z-100 items-center p-4 text-xs sm:text-sm bg-neutral text-neutral-content">
       <nav className="flex items-center w-full justify-between">
         <div className="flex items-center gap-2 wrap">
           <IconCone2 size="24" className="sm:hidden" />
           <IconCone2 size="32" className="hidden sm:block" />
-          <p>Metronomes </p>
+          <p className="font-semibold">Metronomes </p>
           <Link href="/privacy" className="pl-2">
             Privacy Policy
           </Link>
         </div>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6">
           <a
             target="_blank"
             href="https://www.instagram.com/flotischoti/"
             className="link"
           >
-            <IconBrandInstagram />
+            <IconBrandInstagram size="32" className="hidden sm:block" />
+            <IconBrandInstagram size="24" className="sm:hidden" />
           </a>
           <a
             target="_blank"
             href="https://github.com/flotischoti"
             className="link"
           >
-            <IconBrandGithub />
+            <IconBrandGithub size="32" className="hidden sm:block" />
+            <IconBrandGithub size="24" className="sm:hidden" />
           </a>
         </div>
       </nav>
